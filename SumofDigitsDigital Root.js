c9a3688485fe6a1b45e75b23f03ed216d8e16016
Sully Martinez <47094201+SullyMA@users.noreply.github.com>
2023-04-06 23:08:42 -0400

    Create SumofDigitsDigital Root.js

---
+++ SumofDigitsDigital Root.js
@@ -0,0 +1,42 @@
+// Digital root is the recursive sum of all the digits in a number.
+
+// Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+// Examples
+//     16  -->  1 + 6 = 7
+//    942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+// 132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+// 493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11  -->  1 + 1 = 2
+
+function digitalRoot(n) {
+  
+  let arr = (""+n).split('').reduce((c, currentValue) => c += parseInt(currentValue), 0)
+  
+  if(arr >= 10){
+    return digitalRoot(arr)
+  }
+  return arr
+}
+
+
+//different approach
+
+
+function digital_root(n) {
+  if (n < 10) return n;
+  
+  return digital_root(
+    n.toString().split('').reduce(function(acc, d) { return acc + +d; }, 0));
+}
+
+//different approach
+
+function digital_root(n) {
+  if (n < 10)
+    return n;
+
+  for (var sum = 0, i = 0, n = String(n); i < n.length; i++)
+    sum += Number(n[i]);
+   
+  return digital_root(sum);
+}
