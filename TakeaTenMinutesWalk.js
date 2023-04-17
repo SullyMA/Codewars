1bf3997826ecb1ac158f2f0a891869ca6f508369
Sully Martinez <47094201+SullyMA@users.noreply.github.com>
2023-04-17 10:58:25 -0400

    Create TakeaTenMinutesWalk.js

---
+++ TakeaTenMinutesWalk.js
@@ -0,0 +1,44 @@
+// You live in the city of Cartesia where all roads are laid out in a perfect grid. You arrived ten minutes too early to an appointment, so you decided to take the opportunity to go for a short walk. The city provides its citizens with a Walk Generating App on their phones -- everytime you press the button it sends you an array of one-letter strings representing directions to walk (eg. ['n', 's', 'w', 'e']). You always walk only a single block for each letter (direction) and you know it takes you one minute to traverse one city block, so create a function that will return true if the walk the app gives you will take you exactly ten minutes (you don't want to be early or late!) and will, of course, return you to your starting point. Return false otherwise.
+
+// Note: you will always receive a valid array containing a random assortment of direction letters ('n', 's', 'e', or 'w' only). It will never give you an empty array (that's not a walk, that's standing still!).
+
+
+function isValidWalk(walk) {
+
+  let northSouth = 0
+  let westEast = 0
+
+  for(let i = 0; i < walk.length; i++){
+    if(walk[i] === 'n'){
+     northSouth++
+    } else if (walk[i] === 's'){
+      northSouth--
+    } else if(walk[i] === 'w'){
+      westEast++
+    } else if(walk[i] === 'e'){
+      westEast--
+    } 
+  }
+  return walk.length === 10 && northSouth === 0 && westEast === 0;
+}
+
+
+//different approach
+
+function isValidWalk(walk) {
+  const north = walk.filter(item => { return item === "n" }).length;
+  const south = walk.filter(item => { return item === "s" }).length;
+  const east = walk.filter(item => { return item === "e" }).length;
+  const west = walk.filter(item => { return item === "w" }).length;
+  
+  return walk.length === 10 && north === south && east === west;
+}
+
+//or ...
+
+function isValidWalk(walk) {
+  function count(val) {
+    return walk.filter(function(a){return a==val;}).length;
+  }
+  return walk.length==10 && count('n')==count('s') && count('w')==count('e');
+}
